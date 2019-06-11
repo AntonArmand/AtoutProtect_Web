864535df207dcf045199219cf31fe6ccab8a68f3
Armand <armand.anton@limayrac.fr>
2019-06-11 13:27:07 +0200

    reupload index

---
+++ index.php
@@ -0,0 +1,490 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="includes/img/favicon.png" type="image/png">
+	<title>Atout Protect</title>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="includes/css/bootstrap.css">
+	<link rel="stylesheet" href="includes/vendors/linericon/style.css">
+	<link rel="stylesheet" href="includes/css/font-awesome.min.css">
+	<link rel="stylesheet" href="includes/vendors/owl-carousel/owl.carousel.min.css">
+	<link rel="stylesheet" href="includes/css/magnific-popup.css">
+	<link rel="stylesheet" href="includes/vendors/nice-select/css/nice-select.css">
+	<link rel="stylesheet" href="includes/vendors/animate-css/animate.css">
+	<link rel="stylesheet" href="includes/vendors/flaticon/flaticon.css">
+	<!-- main css -->
+	<link rel="stylesheet" href="includes/css/style.css">
+</head>
+
+<body>
+
+<?php
+include_once 'app/views/header.php';
+?>
+
+	<!--================Header Menu Area =================-->
+
+	<!--================Home Banner Area =================-->
+	<section class="home_banner_area">
+		<div class="banner_inner">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5">
+						<div class="banner_content">
+							<h2>
+								La solution <br>
+								sécurité avancée
+							</h2>
+							<p>
+								Atout Protect est une technologie de protection 
+								anti-piratage permetant de sécuriser vos programmes
+							</p>
+							<div class="d-flex align-items-center">
+								<a class="primary_btn" href="#"><span>Nos offres</span></a>
+								<a id="play-home-video" class="video-play-button" href="https://www.youtube.com/watch?time_continue=2&v=J9YzcEe29d0">
+									<span></span>
+								</a>
+								<div class="watch_video text-uppercase">
+									Découvrir
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-7">
+						<div class="home_right_img">
+							<img class="img-fluid" src="img/banner/home-right.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Home Banner Area =================-->
+
+	<!--================Start Features Area =================-->
+	<section class="section_gap features_area">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<h2>Leader dans la sécurisation des programmes</h2>
+						<p>Notre objectif est de vous fournir une solution de sécurité complète et fiable. Nous proposons trois gammes différentes de produit afin de répondre au mieux à votre besoin.</p>
+						<a href="#" class="primary_btn"><span>Découvrir nos offres!!</span></a>
+					</div>
+				</div>
+			</div>
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<div class="left_features">
+						<img class="img-fluid" src="includes/img/mainFormApp.JPG" alt="">
+					</div>
+				</div>
+				<div class="col-lg-5 offset-lg-1">
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+							<h4>Choisir votre solution</h4>
+						</div>
+						<div class="feature_content">
+							<p>En fonction de vos besoins, choisissez la solution la plus adaptée.</p>
+						</div>
+					</div>
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+							<h4>Paiement</h4>
+						</div>
+						<div class="feature_content">
+							<p>Le paiement est 100% sécurisé via Paypal et AlloPass.</p>
+						</div>
+					</div>
+					<!-- single features -->
+					<div class="single_feature">
+						<div class="feature_head">
+							<span class="lnr lnr-screen"></span>
+							<h4>Installer</h4>
+						</div>
+						<div class="feature_content">
+							<p>Le paiement validé et confirmé, téléchargez la solution.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Features Area =================-->
+
+	<!--================Recent Update Area =================-->
+	<section class="recent_update_area">
+		<div class="container">
+			<div class="recent_update_inner">
+				<ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+					<li class="nav-item">
+						<a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">
+							<span class="lnr lnr-screen"></span>
+							<h6>Design Trends</h6>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"
+						 aria-selected="false">
+						 <span class="lnr lnr-screen"></span>
+						 <h6>Development</h6>
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact"
+						 aria-selected="false">
+						 <span class="lnr lnr-screen"></span>
+						 <h6>Delivery</h6>
+						</a>
+					</li>
+				</ul>
+				<div class="tab-content" id="myTabContent">
+					<div class="tab-pane fade" id="home" role="tabpanel" aria-labelledby="home-tab">
+						<div class="row recent_update_text align-items-center">
+							<div class="col-lg-5">
+								<div class="common_style">
+									<p class="line">Modern Daesign Trends</p>
+									<h3>We Believe that <br /> Inner beauty Lasts Long</h3>
+									<p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that as women.</p>
+									<a class="primary_btn" href="#"><span>Learn More</span></a>
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<div class="chart_img">
+									<img class="img-fluid" src="includes/img/chart.png" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+						<div class="row recent_update_text align-items-center">
+							<div class="col-lg-5">
+								<div class="common_style">
+									<p class="line">Modern Daesign Trends</p>
+									<h3>We Believe that <br /> Inner beauty Lasts Long</h3>
+									<p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that as women.</p>
+									<a class="primary_btn" href="#"><span>Learn More</span></a>
+								</div>
+							</div>
+							<div class="col-lg-6">
+								<div class="chart_img">
+									<img class="img-fluid" src="includes/img/chart.png" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane fade show active" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+						<div class="row recent_update_text align-items-center">
+							<div class="col-lg-5">
+								<div class="common_style">
+									<p class="line">Modern Daesign Trends</p>
+									<h3>We Believe that <br /> Inner beauty Lasts Long</h3>
+									<p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that as women.</p>
+									<a class="primary_btn" href="#"><span>Learn More</span></a>
+								</div>
+							</div>
+							<div class="col-lg-6 text-right">
+								<div class="chart_img">
+									<img class="img-fluid" src="includes/img/chart.png" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================ Recent Update Area =================-->
+
+	<!--================Start Big Features Area =================-->
+	<section class="section_gap big_features">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<h2>Amazing Features That make it Awesome!</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row features_content">
+				<div class="col-lg-4 offset-lg-1">
+					<div class="big_f_left">
+						<img class="img-fluid" src="includes/img/f-img1.png" alt="">
+					</div>
+				</div>
+				<div class="col-lg-4 offset-lg-2">
+					<div class="common_style">
+						<p class="line">Modern Daesign Trends</p>
+						<h3>We Believe that <br /> Inner beauty Lasts Long</h3>
+						<p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that as women.</p>
+						<a class="primary_btn" href="#"><span>Learn More</span></a>
+					</div>
+				</div>
+				<div class="border-line"></div>
+				<img class="shape1"src="img/shape1.png" alt="">		
+				<img class="shape2"src="img/shape2.png" alt="">		
+				<img class="shape3"src="img/shape1.png" alt="">		
+			</div>
+
+			<div class="row features_content bottom-features">
+				<div class="col-lg-5">
+					<div class="common_style">
+						<p class="line">Modern Daesign Trends</p>
+						<h3>We Believe that <br /> Inner beauty Lasts Long</h3>
+						<p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that as women.</p>
+						<a class="primary_btn" href="#"><span>Learn More</span></a>
+					</div>
+				</div>
+				<div class="col-lg-5 offset-lg-2">
+					<div class="big_f_left">
+						<img class="img-fluid" src="includes/img/f-img2.png" alt="">
+					</div>
+				</div>
+				<div class="border-line"></div>
+				<img class="shape1"src="includes/img/shape1.png" alt="">		
+				<img class="shape2"src="includes/img/shape2.png" alt="">		
+				<img class="shape3"src="includes/img/shape1.png" alt="">		
+			</div>
+		</div>
+	</section>
+	<!--================End Big Features Area =================-->
+	
+	<!--================Srart Pricing Area =================-->
+	<section class="price_area section_gap">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<h2>Notre gamme de produits</h2>
+						<p>Choisissez la version la plus appropriée à vos besoins. Pour plus de question n'hésitez pas nous contacter afin que des experts puissent vous répondre.</p>
+					</div>
+				</div>
+			</div>
+			<div class="price_inner row justify-content-center">
+				<div class="col-lg-4 col-md-6">
+					<div class="price_item">
+						<div class="price_head">
+							<h4>1 mois</h4>
+						</div>
+						<div class="price_body">
+							<ul class="list">
+								<li><a href="#">Support pendant 1 mois</a></li>
+								<li><a href="#">Compatible C#</a></li>
+								<li><a href="#">Paiement sécurisé</a></li>
+							</ul>
+						</div>
+						<div class="price_footer">
+							<h3>199<span class="dlr">€</span></h3>
+							<a class="primary_btn" href="#"><span>Acheter</span></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="price_item">
+						<div class="price_head">
+							<h4>Illimité</h4>
+						</div>
+						<div class="price_body">
+							<ul class="list">
+								<li><a href="#">Support pendant 1 mois</a></li>
+								<li><a href="#">Compatible C#</a></li>
+								<li><a href="#">Paiement sécurisé</a></li>
+							</ul>
+						</div>
+						<div class="price_footer">
+							<h3>1499<span class="dlr">€</span></h3>
+							<a class="primary_btn" href="#"><span>Acheter</span></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="price_item">
+						<div class="price_head">
+							<h4>6 mois</h4>
+						</div>
+						<div class="price_body">
+							<ul class="list">
+								<li><a href="#">Support pendant 1 mois</a></li>
+								<li><a href="#">Compatible C#</a></li>
+								<li><a href="#">Paiement sécurisé</a></li>
+							</ul>
+						</div>
+						<div class="price_footer">
+							<h3>699<span class="dlr">€</span></h3>
+							<a class="primary_btn" href="#"><span>Acheter</span></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================End Pricing Area =================-->
+
+	<!--================ Start Testimonial Area =================-->
+	<div class="section_gap_top testimonial_area">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-lg-8 text-center">
+					<div class="main_title">
+						<h2>Ce que nos clients disent de nous</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-lg-6 text-center">
+					<div class="active_testimonial owl-carousel" data-slider-id="1">
+						<!-- single testimonial -->
+						<div class="single_testimonial">
+							<div class="testimonial_head">
+								<img src="includes/img/quote.png" alt="">
+								<h4>Bristan Chell</h4>
+								<div class="review">
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+								</div>
+							</div>
+							<div class="testimonial_content">
+								<p>Je cherchais une solution afin de sécuriser mon programme. AtouProtect répond parfaitement à mes attentes pour un prix abordable.</p>
+							</div>
+						</div>
+
+						<div class="single_testimonial">
+							<div class="testimonial_head">
+								<img src="includes/img/quote.png" alt="">
+								<h4>Hella Alexeeva</h4>
+								<div class="review">
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+								</div>
+							</div>
+							<div class="testimonial_content">
+								<p>Produit conforme à la description. Cette solution clef en main nous à permis de sécuriser notre application très rapidement !!</p>
+							</div>
+						</div>
+
+						<div class="single_testimonial">
+							<div class="testimonial_head">
+								<img src="includes/img/quote.png" alt="">
+								<h4>Joseph Gorshkov</h4>
+								<div class="review">
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+								</div>
+							</div>
+							<div class="testimonial_content">
+								<p>Excellent produit !! assistance au top.</p>
+							</div>
+						</div>
+
+						<div class="single_testimonial">
+							<div class="testimonial_head">
+								<img src="includes/img/quote.png" alt="">
+								<h4>Veronica Avdeeva</h4>
+								<div class="review">
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+									<i class="fa fa-star"></i>
+								</div>
+							</div>
+							<div class="testimonial_content">
+								<p>Produit simplement parfait !! Je recommande.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--================ End Testimonial Area =================-->
+
+
+	<!--================Footer Area =================-->
+	<footer class="footer_area section_gap">
+            <div class="container">
+                <div class="row footer_inner">
+                    <div class="col-lg-5 col-sm-6">
+                        <aside class="f_widget ab_widget">
+                            <div class="f_title">
+                                <h3>A Propos</h3>
+                            </div>
+                            <p>Projet réalisé dans le cadre du Bachelor RPI<br>Promo Engelbart 2018 - 2019.</p>
+                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> Tous droits réservés</p>
+                        </aside>
+                    </div>
+                    <div class="col-lg-5 col-sm-6">
+                        <aside class="f_widget news_widget">
+                            <div class="f_title">
+                                <h3>Newsletter</h3>
+                            </div>
+                            <p>Se tenir informé des nouveautés</p>
+                            <div id="mc_embed_signup">
+                                <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscribe_form relative">
+                                    <div class="input-group d-flex flex-row">
+                                        <input name="EMAIL" placeholder="Entrez votre adresse email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
+                                        <button class="btn sub-btn"><span class="lnr lnr-arrow-right"></span></button>      
+                                    </div>              
+                                    <div class="mt-10 info"></div>
+                                </form>
+                            </div>
+                        </aside>
+                    </div>
+                    <div class="col-lg-2">
+                        <aside class="f_widget social_widget">
+                            <div class="f_title">
+                                <h3>Nous suivre</h3>
+                            </div>
+                            <p>Nos réseaux</p>
+                            <ul class="list">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                            </ul>
+                        </aside>
+                    </div>
+                </div>
+            </div>
+        </footer>
+	<!--================End Footer Area =================-->
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="includes/js/jquery-3.2.1.min.js"></script>
+	<script src="includes/js/popper.js"></script>
+	<script src="includes/js/bootstrap.min.js"></script>
+	<script src="includes/js/stellar.js"></script>
+	<script src="includes/js/jquery.magnific-popup.min.js"></script>
+	<script src="includes/vendors/nice-select/js/jquery.nice-select.min.js"></script>
+	<script src="includes/vendors/isotope/imagesloaded.pkgd.min.js"></script>
+	<script src="includes/vendors/isotope/isotope-min.js"></script>
+	<script src="includes/vendors/owl-carousel/owl.carousel.min.js"></script>
+	<script src="includes/js/jquery.ajaxchimp.min.js"></script>
+	<script src="includes/vendors/counter-up/jquery.waypoints.min.js"></script>
+	<script src="includes/vendors/counter-up/jquery.counterup.min.js"></script>
+	<script src="includes/js/mail-script.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="includes/js/gmaps.min.js"></script>
+	<script src="includes/js/theme.js"></script>
+</body>
+
+</html>
